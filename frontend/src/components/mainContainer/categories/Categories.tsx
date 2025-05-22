176a87f4e565360725861da9db9a3e63b7c3539d
serkosal <s3rkosal@gmail.com>
2025-05-22 23:43:17 +0400

    Refactor categories.tsx RadioButtons creation

--- frontend/src/components/mainContainer/categories/Categories.tsx
+++ frontend/src/components/mainContainer/categories/Categories.tsx
@@ -2,45 +2,75 @@ import React, { useState } from "react";
 
 import "./categories.css"
 
-export default function Categories(){
+interface IRadioButton {
+    id: string;
+    value: string;
+    checked?: boolean;
+}
 
-    const [category, setCategory] = useState("All");
+interface IRadioButtonsProps {
+    classname?: string;
+    legend?: string;
+
+    name: string;
+    variants: IRadioButton[];
+
+    setCategory: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function RadioButtons({
+    classname,
+    legend,
+    name,
+    variants,
+    setCategory
+} : IRadioButtonsProps) {
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setCategory(e.target.value);
     };
 
-    const variants = [];
-    for (let variant of [
-        {id: "All-cat",         value: "All", checked: category == "All"},
-        {id: "CPU-cat",         value: "CPU"},
-        {id: "GPU-cat",         value: "GPU"},
-        {id: "Motherboard-cat", value: "Motherboard"},
-        {id: "PSU-cat",         value: "PSU"},
-        {id: "RAM-cat",         value: "RAM"},
-        {id: "Data-cat",        value: "Data"},
-    ]) {
-        variants.push(
-            <div>
-                <input 
-                    type="radio" 
-                    id={variant.id}
-                    name="category" 
-                    value={variant.value} 
-                    checked={variant.checked}
-                    onChange={handleChange}
-                />
-                <label htmlFor={variant.id}>{variant.value}</label>
-            </div>
-        )
-    }
+    const ret_variants = variants.map((variant) => (
+        <div key={variant.id}>
+            <input
+                type="radio"
+                id={variant.id}
+                name={name}
+                value={variant.value}
+                checked={variant.checked}
+                onChange={handleChange}
+            />
+            <label htmlFor={variant.id}>{variant.value}</label>
+        </div>
+    ));
 
-    return <div className="categories">
-        <fieldset>
-            <legend>Select a product category:</legend>
+    return <fieldset className={classname}>
+        {legend}
 
-            {variants}
-        </fieldset>
+        {ret_variants}
+    </fieldset>;
+}
 
-    </div>
+export default function Categories(){
+
+    const [category, setCategory] = useState("All");
+
+    const props: IRadioButtonsProps = {
+        classname: "categories",
+        legend: "Choose category",
+        name: "category",
+        setCategory: setCategory,
+        variants: [
+            {id: "All-cat",         value: "All", checked: category == "All"},
+            {id: "CPU-cat",         value: "CPU"},
+            {id: "GPU-cat",         value: "GPU"},
+            {id: "Motherboard-cat", value: "Motherboard"},
+            {id: "PSU-cat",         value: "PSU"},
+            {id: "RAM-cat",         value: "RAM"},
+            {id: "Data-cat",        value: "Data"},
+        ]
+    };
+
+    return <RadioButtons {...props} />
+    
 }
